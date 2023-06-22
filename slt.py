2213b96cf6bbcc384e13eb785a813c8ceb51762c
vandan08 <127378451+vandan08@users.noreply.github.com>
2023-06-22 17:25:28 +0530

    Create slt.py
    string + list + tuples 
    
    methods and all basics about slt

---
+++ slt.py
@@ -0,0 +1,94 @@
+str = "Python"
+# for x in str:
+# 	print(x)
+
+# if "P" in str:
+# 	print("Yes")
+# else:
+# 	print("NO")
+
+# print("1st character :",str[0] )
+# print("2st character :",str[-4] )
+
+
+# slice a string
+
+# print(str[0:3])
+# print(str[-4:-1])
+
+
+# print(str[:3])
+# print(str[3:])
+
+				# methods of string
+
+# print(len(str))
+
+# new_str = str.replace("t" , "T")
+# print(new_str)
+
+
+# p = " p y t hon"
+# print(p.strip())
+
+# print(str.upper(),str.lower())
+
+
+
+					# List
+
+lst = [10,20,30,40]
+# print(lst)
+
+data = ["Python" , 2.23 , 2020]
+# print(data)
+
+# for x in lst:
+# 	print(x)
+# for y in data:
+# 	print(y)
+
+# numbers = [10, 20, 30]
+# #Write your code here
+# sum1 = 0
+# for x in numbers:
+#     sum1 += x
+
+# print(sum(numbers)) // sum is a function 
+
+
+# to copy a list 
+
+# copy_list = lst[:]
+# print(copy_list)
+
+# to empty a list 
+
+data2 = [10,32.23,21]
+# data2[:] = []
+# print(data2)
+
+# c = lst + data 
+# print(c)
+
+# append method 
+# data2.append("wed")
+# data2.insert(2,23)
+# print(data2)
+
+# data2.remove(10)
+# print(data2)
+
+# data2.clear()
+# print(data2)
+
+
+
+					# Tuple
+
+t = (10,20,30)
+print(t)
+
+
+
+
